3bfbad1550973982768e188b6daece07bc2cb685
ronghl <huilin_rong@163.com>
2021-03-16 23:53:18 +0800

    axios封装ing

--- src/main.js
+++ src/main.js
@@ -1,19 +1,61 @@
 import Vue from 'vue'
-import Antd from 'ant-design-vue';
+import Vuex from 'vuex'
 import App from './App';
 import 'ant-design-vue/dist/antd.css';
 import router from './router'
+import Antd, {message} from 'ant-design-vue';
+
 Vue.config.productionTip = false
 
 Vue.use(Antd)
+Vue.use(Vuex)
 
 import moment from 'moment'
-Vue.prototype.$moment=moment
+
+Vue.prototype.$moment = moment
 
 import axios from 'axios'
-Vue.prototype.axios=axios
+
+Vue.prototype.axios = axios
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
+
+axios.defaults.timeout = 10000
+axios.defaults.headers.post['Content-Type'] = "application/json;charset=UTF-8"
+axios.interceptors.request.use(
+    config => {
+        const token = Vue.store.state.token
+        token && (config.headers.Authorization = token)
+        return config
+    },
+    error => {
+        return Promise.error(error)
+    }
+)
+
+// 响应拦截器
+axios.interceptors.response.use(
+    response => {
+        // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据
+        // 否则的话抛出错误
+        let data = response.data
+        if (data && data.code === 1) {
+            return Promise.resolve(response);
+        } else {
+            message.warn(data.msg)
+            return Promise.reject(response);
+        }
+    },
+    // 服务器状态码不是2开头的的情况
+    // 这里可以跟你们的后台开发人员协商好统一的错误状态码
+    // 然后根据返回的状态码进行一些操作，例如登录过期提示，错误提示等等
+    // 下面列举几个常见的操作，其他需求可自行扩展
+    error => {
+        if (error.response.code) {
+            console.log(error)
+        }
+    }
+)
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -10,7 +10,8 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 import {Line} from '@antv/g2plot';
 
-var dayReqCnt = null
+var dayRequestDataList = []
+var dayReqCnt
 
 export default {
   name: 'Statistic',
@@ -20,56 +21,52 @@ export default {
   data() {
     return {
       currentDay: this.$moment().format('YYYY-MM-DD'),
-      requestCountList: [],
+      serverInfo: null,
       dayRequestId: null
     }
   },
   created() {
     console.log("进入created")
-    let dayRequestCountStr = sessionStorage.getItem('day-request-count-list')
-    if (dayRequestCountStr && dayRequestCountStr.length > 0) {
-      this.requestCountList = JSON.parse(dayRequestCountStr)
-    }
     this.getDayRequestCount()
   },
   mounted() {
     console.log("进入mounted")
-    if (!dayReqCnt) {
-      dayReqCnt = new Line('day-req-cnt', {
-        data: this.requestCountList,
-        xField: 'second',
-        yField: 'count'
-      })
-    }
+    dayReqCnt = new Line('day-req-cnt', {
+      dayRequestDataList,
+      xField: 'time',
+      yField: 'count'
+    })
+    dayReqCnt.render()
+
   },
   methods: {
     // 停止实时请求
     stopRealTimeRequest() {
-      sessionStorage.setItem('day-request-count-list', JSON.stringify(this.requestCountList))
       clearTimeout(this.dayRequestId)
     },
-    renderDayRequestCount() {
-      dayReqCnt.render()
+    renderDayRequestCount(num) {
+      let max = 100
+      if (dayRequestDataList.length >= max) {
+        // 移除第一个
+        dayRequestDataList.splice(0, 1)
+      }
+      dayRequestDataList.push({time: this.$moment().format('mm:ss'), count: num})
+      dayReqCnt.changeData(dayRequestDataList)
     },
     getDayRequestCount() {
-      let _requestCountList = this.requestCountList
       let _this = this
       this.axios.get('http://localhost:8128/statistic/website/dayRequest').then(resp => {
         let result = resp.data
         if (result.code === 1) {
-          let max = 5
-          if (_requestCountList.length >= max) {
-            // 移除第一个
-            _requestCountList.splice(0, 1)
-          }
-          _requestCountList.push({second: this.$moment().format('mm:ss'), count: result.data})
-          console.log('result', JSON.stringify(_requestCountList))
-          _this.renderDayRequestCount()
+          _this.renderDayRequestCount(result.data)
           _this.dayRequestId = setTimeout(function () {
             _this.getDayRequestCount()
           }, 2000)
         }
       })
+    },
+    getServerInfo() {
+      this.axios.get('')
     }
   },
   destroyed() {
